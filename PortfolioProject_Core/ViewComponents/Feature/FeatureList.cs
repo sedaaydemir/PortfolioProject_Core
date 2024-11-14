5c6e6fc1266cad5081b0575603a8aea9963e53a6
Seda Aydemir <seda.aydemir7@gmail.com>
2024-11-14 16:46:54 +0300

    Add project files.

---
+++ PortfolioProject_Core/ViewComponents/Feature/FeatureList.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject_Core.ViewComponents.Feature
+{
+    public class FeatureList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+        return View(); 
+        }
+    
+    }
+}
