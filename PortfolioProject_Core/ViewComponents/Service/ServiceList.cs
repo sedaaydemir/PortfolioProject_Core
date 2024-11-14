ee0e5413b4ebc7c0b1f20ed18f30cd25513eb471
Seda Aydemir <seda.aydemir7@gmail.com>
2024-11-14 21:32:02 +0300

    ...

---
+++ PortfolioProject_Core/ViewComponents/Service/ServiceList.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject_Core.ViewComponents.Service
+{
+    public class ServiceList:ViewComponent
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfserviceDal());
+        public IViewComponentResult Invoke()
+        {
+
+            var values = serviceManager.TGetList();
+            return View(values);
+        }
+    
+    }
+}
